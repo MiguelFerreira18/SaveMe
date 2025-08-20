8eb77005ee0beb1ca62fa352adac25f601654e97
Miguel[1230199] <1230199@isep.ipp.pt>
2025-08-20 17:24:04 +0100

    feat: add calendar component

---
+++ public/src/components/calendar/MonthlyCalendar.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  getToday: {
+    type: Function,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['update:valueModel'])
+
+function incrementAYear(dateStr) {
+  const date = new Date(dateStr)
+  date.setFullYear(date.getFullYear() + 1)
+  const currentDatePlusYear = date.toISOString().slice(0, 10)
+  return currentDatePlusYear
+}
+
+function updateDate(value) {
+  emit('update:modelValue', value.target.value)
+}
+
+console.log(props.getToday())
+</script>
+
+<template>
+  <input
+    type="date"
+    :name="name"
+    :id="name"
+    :value="props.modelValue"
+    min="1927-01-01"
+    :max="incrementAYear(props.getToday())"
+    @input="updateDate"
+    class="w-full max-w-md p-2 border border-gray-300 rounded text-center text-base"
+  />
+</template>
+<style scoped></style>
