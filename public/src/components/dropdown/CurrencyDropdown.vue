9bee7a0db57ac893c6a914933309e958e8503072
Miguel[1230199] <1230199@isep.ipp.pt>
2025-08-20 17:24:59 +0100

    feat: add dropdown component

---
+++ public/src/components/dropdown/CurrencyDropdown.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Array,
+    required: true,
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+
+function updateOption(val) {
+  emit('update:modelValue', val)
+}
+</script>
+
+<template>
+  <select
+    :name="label"
+    :id="label"
+    class="w-full max-w-md p-2 border border-gray-300 rounded text-center text-base"
+  >
+    <option
+      v-for="currency in data"
+      :key="currency.id"
+      :value="currency.id"
+      @click="updateOption(currency.id)"
+    >
+      {{ currency.symbol }}
+    </option>
+  </select>
+</template>
+<style scoped></style>
